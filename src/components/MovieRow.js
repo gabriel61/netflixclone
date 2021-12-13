55dd935032bab23e6e2a3b0c3c099f5a3620982b
gabriel61 <sogabris@gmail.com>
2021-12-12 23:38:48 -0300

    Adiciona lista de filmes

---
+++ src/components/MovieRow.js
@@ -0,0 +1,28 @@
+//LINHA DE FILMES
+
+import React from "react";
+import './MovieRow.css'
+
+export default ({title, items}) => {
+    return (
+        <div>
+            <h2>{title}</h2>
+            {/* area da lista */}
+            <div className="movieRow--listarea">
+                <div className="movieRow--list">
+                    {/* >0 ou seja, se houver filmes para mostrar ele vai da um map na lista.
+                    um loop dentro do loop das listas para mostrar os filmes */}
+                    {items.results?.length > 0 && items.results.map((item, key)=>(
+                        <div key={key} className="movieRow--item">
+                            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
+                        </div>
+                        
+                    ))}
+                </div>
+
+                
+                
+            </div>
+        </div>
+    );
+}
