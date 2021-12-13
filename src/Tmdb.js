0977591fb59e889dc9851014f074f945635a79a5
gabriel61 <sogabris@gmail.com>
2021-12-13 00:04:16 -0300

    Corrige top_rated

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -47,7 +47,7 @@ export default {
             {
                 slug: 'toprated',
                 title: 'Em Alta',
-                items: await basicFetch(`/move/top-rated?language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'Action',
