d5572f02d7a5fb7dabf855fd5d9c5b1c7ebcc473
gabriel61 <sogabris@gmail.com>
2021-12-14 02:01:27 -0300

    Adiciona FeaturedMovie

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Tmdb from "./Tmdb";
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
   //useEffect executa a função quando a tele é carregada.
   useEffect(() => {
@@ -13,6 +15,14 @@ export default () => {
       //Pegando a lista TOTAL
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      //Pegando o filme destaque que tem slug = originals
+      let originals = list.filter(i=>i.slug === 'originals');
+      //gerando um numero aleatorio dentro da qtdd max de originals
+      let randomChosen = Math.floor(Math.random()*(originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
     }
 
     loadAll();
@@ -23,9 +33,14 @@ export default () => {
     //header - destaque - as listas - footer
     <div className="page">
 
+      {/* quando FeaturedData existir a gente manda o componente */}
+      {featuredData &&
+        <FeaturedMovie item={featuredData}/>
+      }
+
       <section className="lists">
         {movieList.map((item, key) => (
-          <MovieRow key={key} title={item.title} items={item.items}/>
+          <MovieRow key={key} title={item.title} items={item.items}></MovieRow>
         ))}
       </section>
     </div>
--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -13,7 +13,7 @@ export default ({title, items}) => {
                 <div className="movieRow--list">
                     {/* >0 ou seja, se houver filmes para mostrar ele vai da um map na lista.
                     um loop dentro do loop das listas para mostrar os filmes */}
-                    {items.results?.length > 0 && items.results.map((item, key)=>(
+                    {items.results.length > 0 && items.results.map((item, key)=>(
                         <div key={key} className="movieRow--item">
                             <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
                         </div>
